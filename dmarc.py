8e883b74006cc239030c9e90b47d2296a5f32dd0
Ariel Romeo <arielromeo@gmail.com>
2022-07-01 09:30:35 -0300

    little refactor

--- dmarc.py
+++ dmarc.py
@@ -1,58 +1,19 @@
 #!/usr/bin/env python3
-import xmltodict
-from datetime import datetime
-import pandas
+import argparse
 import gzip
 import zipfile
-import argparse
+from datetime import datetime
+
+import pandas
+import xmltodict
 from tabulate import tabulate
 
 
-class DmarcInfo(object):
-    DATE_FORMAT: str = '%Y-%m-%d'
-    NUMERIC_COLUMNS: list = [8, 10]
-
-    def __init__(self, dmarc: dict):
-        metadata: dict = dmarc['feedback']['report_metadata']
-        self.organization = metadata['org_name']
-        self.begin_date = datetime.utcfromtimestamp(int(metadata['date_range']['begin'])).strftime(DmarcInfo.DATE_FORMAT)
-        self.end_date = datetime.utcfromtimestamp(int(metadata['date_range']['end'])).strftime(DmarcInfo.DATE_FORMAT)
-
-        policy_published: dict = dmarc['feedback']['policy_published']
-        self.domain = policy_published['domain']
-        self.adkim = 'strict' if 'adkim' in policy_published and policy_published['adkim'].lower() == 's' else 'relaxed'
-        self.aspf = 'strict' if 'aspf' in policy_published and policy_published['aspf'].lower() == 's' else 'relaxed'
-        self.p = policy_published['p'] if 'p' in policy_published else 'none'
-        self.sp = policy_published['sp'] if 'sp' in policy_published else 'none'
-        self.pct = policy_published['pct'] if 'pct' in policy_published else 'none'
-
-        self.rows = []
-        if type(dmarc['feedback']['record']) == list:
-            reports = sorted(dmarc['feedback']['record'], key=lambda x: int(x['row']['count']), reverse=True)
-        else:
-            reports = [dmarc['feedback']['record']]
-
-        for _report in reports:
-            self.rows.append(DmarcInfoDetail(_report))
-
-    def plain(self, sort_by_column: int = 0, reverse: bool = False):
-        result: list = []
-        base: list = [self.organization, self.begin_date, self.end_date, self.domain,
-                      self.adkim, self.aspf, self.p, self.sp, self.pct]
-
-        for row in self.rows:
-            for _row in row.plain():
-                result.append(base + _row)
-
-        return sorted(result, key=lambda x: (x[sort_by_column] if sort_by_column not in DmarcInfo.NUMERIC_COLUMNS else int(x[sort_by_column])),
-                      reverse=reverse)
-
-    @staticmethod
-    def get_headers():
-        return ["Organization", "Begin", "End", "Domain", "Adkim", "Aspf", "P", "Sp", "Pct"]
-
-
 class DmarcInfoDetail(object):
+    BASIC_HEADERS: list = ["SourceIP", "Count", "Disposition", "Dkim", "SPF", "HeaderFrom", "SPFDomain", "SPFResult"]
+    EXTRA_HEADERS: list = ["DKIMDomain", "DMKIResult", "DKIMSelector"]
+    NUMERIC_COLUMNS: list = ["Count"]
+
     def __init__(self, record):
         row: dict = record['row']
         self.source_ip = row['source_ip']
@@ -94,13 +55,58 @@ class DmarcInfoDetail(object):
         return result
 
     @staticmethod
-    def get_headers(unique: bool=False):
-        return ["SourceIP", "Count", "Disposition", "Dkim", "SPF", "HeaderFrom", "SPFDomain", "SPFResult"] + \
-               (["DKIMDomain", "DMKIResult", "DKIMSelector"] if unique else [])
+    def get_headers(unique: bool = False):
+        return DmarcInfoDetail.BASIC_HEADERS + (DmarcInfoDetail.EXTRA_HEADERS if unique else [])
+
+
+class DmarcInfo(object):
+    DATE_FORMAT: str = '%Y-%m-%d'
+    HEADERS: list = ["Organization", "Begin", "End", "Domain", "Adkim", "Aspf", "P", "Sp", "Pct"]
+    NUMERIC_COLUMNS: list = ["Pct"] + DmarcInfoDetail.NUMERIC_COLUMNS
+
+    def __init__(self, dmarc: dict):
+        metadata: dict = dmarc['feedback']['report_metadata']
+        self.organization = metadata['org_name']
+        self.begin_date = datetime.utcfromtimestamp(int(metadata['date_range']['begin'])).strftime(DmarcInfo.DATE_FORMAT)
+        self.end_date = datetime.utcfromtimestamp(int(metadata['date_range']['end'])).strftime(DmarcInfo.DATE_FORMAT)
+
+        policy_published: dict = dmarc['feedback']['policy_published']
+        self.domain = policy_published['domain']
+        self.adkim = 'strict' if 'adkim' in policy_published and policy_published['adkim'].lower() == 's' else 'relaxed'
+        self.aspf = 'strict' if 'aspf' in policy_published and policy_published['aspf'].lower() == 's' else 'relaxed'
+        self.p = policy_published['p'] if 'p' in policy_published else 'none'
+        self.sp = policy_published['sp'] if 'sp' in policy_published else 'none'
+        self.pct = policy_published['pct'] if 'pct' in policy_published else 'none'
+
+        self.rows = []
+        if type(dmarc['feedback']['record']) == list:
+            reports = sorted(dmarc['feedback']['record'], key=lambda x: int(x['row']['count']), reverse=True)
+        else:
+            reports = [dmarc['feedback']['record']]
+
+        for _report in reports:
+            self.rows.append(DmarcInfoDetail(_report))
+
+    def plain(self, sort_by_column: str = NUMERIC_COLUMNS[0], reverse: bool = False):
+        result: list = []
+        base: list = [self.organization, self.begin_date, self.end_date, self.domain,
+                      self.adkim, self.aspf, self.p, self.sp, self.pct]
+
+        for row in self.rows:
+            for _row in row.plain():
+                result.append(base + _row)
+
+        return sorted(result, key=lambda x: (x[DmarcInfo.get_headers().index(sort_by_column)] if sort_by_column not in DmarcInfo.NUMERIC_COLUMNS else
+                                             int(x[DmarcInfo.get_headers().index(sort_by_column)])),
+                      reverse=reverse)
+
+    @staticmethod
+    def get_headers(unique: bool = False):
+        return DmarcInfo.HEADERS + DmarcInfoDetail.get_headers(unique)
 
 
 parser = argparse.ArgumentParser(description='Process DMARC reports')
-parser.add_argument('-s', '--sort-by-column', help="Sort by column name", default=None, type=str)
+parser.add_argument('-s', '--sort-by-column', help=f"Sort by column name, by default '{DmarcInfo.NUMERIC_COLUMNS[0]}'", default=DmarcInfo.NUMERIC_COLUMNS[0], type=str)
 parser.add_argument('-r', '--reverse', help="Sort in reverse order", default=False, action='store_true')
 parser.add_argument('file')
 
@@ -121,17 +127,13 @@ else:
 
 dmarc = xmltodict.parse(file)
 report: DmarcInfo = DmarcInfo(dmarc)
-headers = DmarcInfo.get_headers() + DmarcInfoDetail.get_headers(True)
-sort_by_column: int = 0
+headers = DmarcInfo.get_headers(True)
 
-if args.sort_by_column in headers:
-    sort_by_column = headers.index(args.sort_by_column)
-elif args.sort_by_column is not None:
+if args.sort_by_column not in headers:
     parser.error(f"The {args.sort_by_column} is not a valid column name")
 
-data = report.plain(sort_by_column, args.reverse)
-headers = DmarcInfo.get_headers() + DmarcInfoDetail.get_headers(True)
-index = DmarcInfo.get_headers() + DmarcInfoDetail.get_headers()
+data = report.plain(args.sort_by_column, args.reverse)
+index = DmarcInfo.get_headers()
 
 df = pandas.DataFrame(data, columns=headers)
 df.set_index(index)
